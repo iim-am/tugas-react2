11f3e6d0353d62e34aea348019fb79a116063eee
iim-am <iim_mafahir@apps.ipb.ac.id>
2020-10-20 15:30:24 +0700

    ini adalah folder src pada tugas react 2

---
+++ src-tugas-react2/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Stopwatch from './Stopwatch';
+
+
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+        <Stopwatch/>
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src-tugas-react2/Stopwatch.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+class Stopwatch extends React.Component {
+    state = {
+      runningTime: 0,
+      isRunning: false
+    };
+    componentDidMount() {
+    }
+    handleStartStopClick = () => {
+      if (this.state.isRunning) {
+        clearInterval(this.timerID);
+        this.setState({ isRunning: false });
+      } else {
+        const startTime = Date.now() - this.state.runningTime;
+        this.timerID = setInterval(() => {
+          this.setState({ runningTime: Date.now() - startTime , isRunning: true});
+        }, 100);
+      }
+    }
+    handleResetClick = () =>{
+      
+        clearInterval(this.timerID);
+        this.setState({ isRunning: false, runningTime: 0 });
+    }
+  
+    componentWillUnmount() {
+      clearInterval(this.timerID);
+    }
+    formatTime(t) {
+      return (t / 1000).toFixed(1);
+    }
+    render() {
+      return (
+        <div>
+          <h1>{this.formatTime(this.state.runningTime)}</h1>
+          <button onClick={this.handleStartStopClick}> {this.state.isRunning?"Stop":"Start"}</button>
+          <button onClick={this.handleResetClick}>Reset</button>
+        </div>
+      );
+    }
+  }
+  //ReactDOM.render(<Stopwatch />, document.getElementById("root"));
+  export default Stopwatch;
